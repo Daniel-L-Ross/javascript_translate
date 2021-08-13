78b87f1bf487eacc2e6e558d481efe688a55ecbd
Dan Ross <daniellross@gmail.com>
2021-08-13 11:54:23 -0500

    finished dice exercise

--- javascriptCode/scripts/main.js
+++ javascriptCode/scripts/main.js
@@ -1,6 +1,8 @@
 import { enemyNames } from "./EnemyNames.js";
 import { placeNames } from "./PlaceNames.js";
+import { rollDice } from "./RollingDice.js";
 
 // console.log("PlaceNames Solution: ")
 // placeNames()
-enemyNames()
+// enemyNames()
+rollDice(10)
